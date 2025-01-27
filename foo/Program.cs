4f399cdeabeafdd0557c1813861eb3247ef151c2
Radmir Baembitov <radmir.baembitov@jetbrains.com>
2025-01-27 12:45:40 +0100

    Initial commit

---
+++ foo/Program.cs
@@ -0,0 +1,5 @@
+﻿int number = default; // Violates the rule. Should prefer assigning 0 instead of default.
+bool isActive = default; // Violates the rule. Should prefer assigning false instead of default.
+string text = default; // Violates the rule. Should prefer assigning null instead of default.
+
+Console.WriteLine($"Number: {number}, IsActive: {isActive}, Text: {text}");
